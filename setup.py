24f6e2967bbcc2af6b629d1c819b61c6f07f81a1
Byrne Reese <byrne@majordojo.com>
2020-11-11 11:06:10 -0800

    Updated prereqs

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.16'
+VERSION = '0.1.17'
 
 setup(
     name="mkdocs-moonstone",
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'mkdocs>=1.0',
-        'mkdocs-git-committers-plugin>=0.2.0',
+        'mkdocs-git-committers-plugin>=0.2.1',
         'mkdocs-bootstrap4>=0.1.5',
         'mkdocs-bootstrap-tables-plugin>=0.1.1',
         'mkdocs-material>=3.0'
